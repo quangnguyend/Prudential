882fdb129cbb1b2cbed2c13db6071ed740520d41
quangnguyend <quang.nguyend@s3corp.com.vn>
2017-05-18 18:17:36 +0700

    Revert "Vuong prudential"

--- www/js/controllers/customer/customer_controller.js
+++ www/js/controllers/customer/customer_controller.js
@@ -6,77 +6,16 @@ function customerCtrl ($scope, $rootScope, DataService, $location, $ionicScrollD
   var customer = DataService.getData('customer') || { lastname: '', firstname: '', smoke: false, phone: ''}
   $scope.customer = customer
 
-  var schema = {
-    customer_name: {
-      lastname: "string",
-      firstname: "string"
-    },
-    customer_dob: {
-      birthday:"object"
-    },
-    customer_job: {
-      nature_occupation:"string",
-    },
-    customer_goal: {
-      goals:"string",
-    },
-    customer_existing: {
-      existing:"string"
-    },
-    customer_smoker: {
-      phone: "string"
-    }
-  }
-
   $scope.clickButtonHandle = function (nextRoute) {
     $location.url(nextRoute)
     DataService.setData('customer', $scope.customer)
   }
 
-  function validationForm(form){
-    var item = schema[form]
-    if(!item)
-      return
-    var rs = Object.keys(item).map(function(field){
-      if(typeof $scope.customer[field] === item[field] && $scope.customer[field] !== '')
-        return true
-      return false
-    }) 
-    if(rs.indexOf(false) >= 0){
-      return false
-    }
-    return true
-  }
-
-  $scope.clickNextHandle = function(idElement, nextIdElement){  
-    if(!nextIdElement)
-      return
-    var nextIdElement = '#' + nextIdElement
-    distance = $(nextIdElement)[0] && $(nextIdElement)[0].offsetTop || 0
-
-    $scope.pageList.forEach(function(page, index, pageList){
-      if(page.id === idElement){
-        if(validationForm(idElement)){
-          if(pageList[index+1]) {
-            pageList[index+1].enable = true
-          }
-        }
-      }
-    })
+  $scope.clickNextHandle = function(classEl){  
+    var idElement = '#' + classEl
+    distance = $(idElement)[0].offsetTop
     $ionicScrollDelegate.scrollTo(0, distance, true)
   }
-
-  $scope.pageList = [
-    {id:'customer_name', path:'templates/customer/customer_name.html', next: 'customer_dob', enable: true, step: 1},
-    {id:'customer_dob', path:'templates/customer/customer_dob.html', next: 'customer_job', enable: false, step:2},
-    {id:'customer_job', path:'templates/customer/customer_job.html', next: 'customer_goal', enable: false, step: 3},
-    {id:'customer_goal', path:'templates/customer/customer_goal.html', next: 'customer_existing', enable: false, step: 4},
-    {id:'customer_existing', path:'templates/customer/customer_existing.html', next: 'customer_smoker', enable: false, step: 5},
-    {id:'customer_smoker', path:'templates/customer/customer_smoker.html', next: 'customer_thanks', enable: false, step: 6},
-    {id:'customer_thanks', path:'templates/customer/customer_thanks.html', next: '', enable: false, step: 7},
-  ]
-
-
 
   $scope.data = {
     nature_occupation: [
