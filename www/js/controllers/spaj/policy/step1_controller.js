2c8ef5b1d2e510a67d1bea7ef85f27a90c07f266
quang.nguyen <quang.nguyend@s3corp.com.vn>
2017-05-24 15:36:39 +0700

    fixed lint

--- www/js/controllers/spaj/policy/step1_controller.js
+++ www/js/controllers/spaj/policy/step1_controller.js
@@ -6,7 +6,6 @@ function step1Ctr ($scope, $rootScope) {
   $rootScope.showBack = false
   $rootScope.showMenu = true
   $rootScope.nextpagePh = function () {
-    console.log('sssss')
   }
   // default view is POLICY
   // $scope.view = '1'
